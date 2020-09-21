21677d891a0142b42c45143f4f02a6b631db3a79
Elijah Williams <ejwill04@gmail.com>
2020-09-21 06:29:04 -0600

    remove routing for now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import React, { useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react';
 
 // components
 import Search from './components/Search';
@@ -12,33 +6,10 @@ import Search from './components/Search';
 // styles
 import './App.css';
 
-const Nav = () => {
-  return (
-    <nav>
-      <div>
-        <Link to='/'>Search</Link>
-      </div>
-    </nav>
-  )
-}
-
-const Routes = () => {
-  return (
-    <Switch>
-      <Route path="/">
-        <Search />
-      </Route>
-    </Switch>
-  )
-}
-
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Nav />
-        <Routes />
-      </Router>
+      <Search />
     </div>
   );
 }
